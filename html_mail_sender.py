a35d8e82e15cac075b32dc0ff0364c973787c960
Shamoel <shamoel.ahmad@gmail.com>
2022-06-01 22:55:04 +0530

    COntent-Transfer-Encoding changes working

--- html_mail_sender.py
+++ html_mail_sender.py
@@ -6,11 +6,12 @@ from getpass import getpass
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import email.mime, email.mime.nonmultipart, email.charset
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 
-password = getpass("Type your password and press enter: ")
+# password = getpass("Type your password and press enter: ")
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -63,18 +64,24 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 # Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+cs=email.charset.Charset('UTF-8')
+cs.body_encoding = email.charset.BASE64
+
+part1=email.mime.nonmultipart.MIMENonMultipart('text', 'plain')
+part1.set_payload(text, charset=cs)
+part2=email.mime.nonmultipart.MIMENonMultipart('text', 'html')
+part2.set_payload(html, charset=cs)
 
 # Add HTML/plain-text parts to MIMEMultipart message
 # The email client will try to render the last part first
 message.attach(part1)
 message.attach(part2)
 
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-	server.login(sender_email, password)
-	server.sendmail(sender_email, receiver_email, message.as_string())
-	server.quit()
+print(message)
 
-print("mail_sent")
+# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+# 	server.login(sender_email, password)
+# 	server.sendmail(sender_email, receiver_email, message.as_string())
+# 	server.quit()
 
+# print("mail_sent")
